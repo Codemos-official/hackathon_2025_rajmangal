7582f46a982786554d14d482c43a8dbad1378582
mangalji <rajmangal03@gmail.com>
2026-01-03 14:44:24 +0530

    all updates in accounts app are updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User 
 
-# Register your models here.
+@admin.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email','username','phone_number','is_email_verified')
+    search_fields = ('email','username','phone_number')
