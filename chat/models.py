bbe998bc083f4b087d43b44dd405181751f10d91
mangalji <rajmangal03@gmail.com>
2026-01-03 14:42:07 +0530

    all app's models updated

--- chat/models.py
+++ chat/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+# from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
-    is_phone_verified = models.BooleanField(default=False)
-    is_email_verified = models.BooleanField(default=False)
+# class User(AbstractUser):
+#     phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+#     is_phone_verified = models.BooleanField(default=False)
+#     is_email_verified = models.BooleanField(default=False)
     
-    def __str__(self):
-        return self.username
+#     def __str__(self):
+#         return self.username
     
 class ChatGroup(models.Model):
     name = models.CharField(max_length=100)
     created_by = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='created_groups'
+        # User, on_delete=models.CASCADE, related_name='created_groups'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_groups'
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -21,7 +23,8 @@ class ChatGroup(models.Model):
     
 class GroupMember(models.Model):
     group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     joined_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -34,13 +37,13 @@ class Message(models.Model):
     )
 
     sender = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='sent_messages'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages'
     )
 
     target_type = models.CharField(max_length=10, choices=TARGET_CHOICES)
 
     target_user = models.ForeignKey(
-        User, null=True, blank=True, on_delete=models.CASCADE, related_name='received_messages'
+        settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name='received_messages'
     )
 
     target_group = models.ForeignKey(
--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from chat import models as chat_md
 
-# Create your models here.
+class ScheduledMessage(models.Model):
+    message = models.OneToOneField(
+        chat_md.Message, on_delete=models.CASCADE
+    )
+
+    scheduled_time = models.DateTimeField()
+    is_sent = models.BooleanField(default=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['scheduled_time']),
+            models.Index(fields=["is_sent"]),
+        ]
